ba4c7c0a1cc15601274d5fe82897c0572e96d426
Ronan <ronanmusic@btinternet.com>
2020-06-22 16:30:00 +0100

    template completed

--- pygame_template.py
+++ pygame_template.py
@@ -1,9 +1,9 @@
 # Pygame template - skeleton for new pygame projects - from www.kidscancode.com.
-import Pygame
+import pygame
 import random
 
 WIDTH = 360 # width of new game window
-HEIGTH = 480 # height ------------
+HEIGHT = 480 # height ------------
 FPS = 30 # frames per second
 
 # define Colors (R, G, B)
@@ -20,17 +20,28 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 
+all_sprites = pygame.sprite.Group()
+
 # Game Loop
 running = True
 while running:
+    #keep loop running at correct speed
+    clock.tick(FPS)
     # Process input (events)
     for event in pygame.event.get():
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
     # Update
+    all_sprites.update()
+
     # Render (draw)
     # Draw / render
     screen.fill(BLACK)
+    all_sprites.draw(screen)
+
     # *after* drawing everything, flip the display
-    pygame.display.flip()    
+    pygame.display.flip()
+
+
+pygame.quit()
